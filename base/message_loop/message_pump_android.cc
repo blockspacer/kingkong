3ae4030cdbe943699777e8fae47d832571914401
test <test>
2020-04-16 13:51:54 +0800

    安卓主线程loop 增加

---
+++ base/message_loop/message_pump_android.cc
@@ -0,0 +1,33 @@
+﻿#include "message_pump_android.h"
+#include "log.h"
+#include "time_util.h"
+#include "thread_util.h"
+#include <boost/assert.hpp>
+#include <boost/timer.hpp>
+BEGIN_NAMESPACE_LOOPER
+
+MessagePumpAndroid::MessagePumpAndroid() : MessagePumpImpl("Main") {
+  BASE_THREAD::SetThreadTls(this);
+  BASE_THREAD::SetCurrentThreadName(name());
+  
+
+
+}
+
+MessagePumpAndroid::~MessagePumpAndroid() {
+}
+
+void MessagePumpAndroid::Wakeup(uint64_t expired_time) {
+  auto now = BASE_TIME::GetTickCount2();
+ 
+}
+
+void MessagePumpAndroid::DoRun() {
+}
+
+void MessagePumpAndroid::DoStop() {
+  
+}
+
+
+END_NAMESPACE_LOOPER
---
+++ base/message_loop/message_pump_android.h
@@ -0,0 +1,27 @@
+﻿#ifndef _BASE_MESSAGE_PUMP_ANDROID_H_
+#define _BASE_MESSAGE_PUMP_ANDROID_H_
+#include "message_pump_impl.h"
+#include <base_header.h>
+
+BEGIN_NAMESPACE_LOOPER
+
+class MessagePumpAndroid :public MessagePumpImpl {
+public:
+  MessagePumpAndroid();
+  ~MessagePumpAndroid();
+
+public:
+
+protected:
+  void Wakeup(uint64_t expired_time) override;
+
+  void DoRun() override;
+
+
+  void DoStop() override;
+
+private:
+};
+
+END_NAMESPACE_LOOPER
+#endif
