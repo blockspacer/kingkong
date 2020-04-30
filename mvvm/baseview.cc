2492671ef36ad5b612eb4943dcedfa17d0d6df6a
test <test>
2020-04-30 15:45:01 +0800

    通知上层属性去掉old value

--- mvvm/baseview.cc
+++ mvvm/baseview.cc
@@ -9,8 +9,8 @@ BaseView::~BaseView() {
 void BaseView::BindVM(int32_t type) {
   vm_ = ViewModel::Create(type);
   //给UI 一个机会绑定事件和属性
-  vm_->BindPropertyChanged([this](int32_t property_id, const boost::any& before_value, const boost::any& after_value) {
-    OnPropertyChanged(property_id, before_value, after_value);
+  vm_->BindPropertyChanged([this](int32_t property_id, const boost::any& after_value) {
+    OnPropertyChanged(property_id, after_value);
   });
 
   vm_->BindEvent([this](int32_t event_id, const boost::any& value) {
